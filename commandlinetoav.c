5f387bb4c5702964488fd8646341afdc5cb3ba21
Stevecmd <murimi101@gmail.com>
2024-02-14 09:54:54 -0800

    Betty compliant commandlinetoav.c

--- commandlinetoav.c
+++ commandlinetoav.c
@@ -5,50 +5,71 @@
 #define MAX_WORDS 100
 #define MAX_LENGTH 100
 
-char **split_string(const char *str, int *num_words) {
-    char **words = malloc(MAX_WORDS * sizeof(char *));
-    if (words == NULL) {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
+/**
+ * split_string - splits a string into words
+ * @str: The string to split
+ * @num_words: Pointer to store the number of words found.
+ *
+ * Return: Array of strings containing the words.
+ */
 
-    *num_words = 0;
+char **split_string(const char *str, int *num_words)
+{
+	char **words = malloc(MAX_WORDS * sizeof(char *));
 
-    char *token = strtok((char *)str, " \t\n");
-    while (token != NULL) {
-        words[*num_words] = malloc((strlen(token) + 1) * sizeof(char));
-        if (words[*num_words] == NULL) {
-            perror("malloc");
-            exit(EXIT_FAILURE);
-        }
+	if (words == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
 
-        strcpy(words[*num_words], token);
+	*num_words = 0;
 
-        (*num_words)++;
+	char *token = strtok((char *)str, " \t\n");
 
-        if (*num_words >= MAX_WORDS) {
-            fprintf(stderr, "Maximum number of words exceeded\n");
-            exit(EXIT_FAILURE);
-        }
+	while (token != NULL)
+	{
+		words[*num_words] = malloc((strlen(token) + 1) * sizeof(char));
+		if (words[*num_words] == NULL)
+		{
+			perror("malloc");
+			exit(EXIT_FAILURE);
+		}
 
-        token = strtok(NULL, " \t\n");
-    }
+		strcpy(words[*num_words], token);
 
-    return words;
+		(*num_words)++;
+
+		if (*num_words >= MAX_WORDS)
+		{
+			fprintf(stderr, "Maximum number of words exceeded\n");
+			exit(EXIT_FAILURE);
+		}
+
+		token = strtok(NULL, " \t\n");
+	}
+	return (words);
 }
 
-int main() {
-    char str[] = "This is a test string";
-    int num_words;
-    char **words = split_string(str, &num_words);
+/**
+ * main - Entry point of the program.
+ *
+ * Return: 0 upon successful execution.
+ */
+int main(void)
+{
+	char str[] = "This is a test string";
+	int num_words;
+	char **words = split_string(str, &num_words);
 
-    printf("Number of words: %d\n", num_words);
-    printf("Words:\n");
-    for (int i = 0; i < num_words; i++) {
-        printf("%s\n", words[i]);
-        free(words[i]);
-    }
+	printf("Number of words: %d\n", num_words);
+	printf("Words:\n");
+	for (int i = 0; i < num_words; i++)
+	{
+		printf("%s\n", words[i]);
+		free(words[i]);
+	}
 
-    free(words);
-    return 0;
+	free(words);
+	return (0);
 }
